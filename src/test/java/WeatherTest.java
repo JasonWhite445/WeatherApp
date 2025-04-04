a0fd1f8d8b0dda631c3f27193a0adad4a952fcf1
Jason White <jason.white@my.simpson.edu>
2025-04-04 17:50:31 -0500

    Completed Weather App

--- src/test/java/WeatherTest.java
+++ src/test/java/WeatherTest.java
@@ -1,3 +1,5 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -13,13 +15,32 @@ public class WeatherTest {
     private AppLauncher app;
 
     @BeforeEach
-    public void SetupTest() {app = new AppLauncher();}
+    public void SetupTest() {
+        app = new AppLauncher();
+    }
 
     @Test
-    public void LaunchWeatherApplication() {
+    public void launchWeatherApplication() {
 
         Assertions.assertNotNull(app.launch(), "WeatherApp is created!");
 
     }
+
+    @Test
+    public void testForecastRetrieval() {
+
+        JSONObject weatherData = WeatherApp.weatherData("New York");
+        Assertions.assertNotNull(weatherData, "Weather Data should provide a valid location");
+
+    }
+
+    @Test
+    public void testLocationRetrieval() {
+
+        JSONArray locationData = WeatherApp.getLocationInfo("Los Angeles");
+        Assertions.assertFalse(locationData.isEmpty(), "Location data retrieved successfully.");
+
+    }
+
 
 }
